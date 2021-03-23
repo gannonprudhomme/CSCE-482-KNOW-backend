c5696d540b52d1801f6bc5a0e02fd77c91cf0d02
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-03-23 16:40:42 -0500

    Added sample test

---
+++ know/rdf/tests/sample.py
@@ -0,0 +1,24 @@
+import unittest
+
+def some_function(val):
+    return val
+
+class SampleTestCase(unittest.TestCase):
+    """ Sample test case """
+    def __init__(self):
+        # You can optionally setup things here that can be used in the rest of the tests
+        pass
+
+    def test_sample_test_returns_5(self):
+        """ Typical test name format is like test_thing_in_situation_does_thing
+            e.g., test_invalid_uri_does_raise_error
+        """
+        
+        # arrange
+        expected = 5
+
+        # act
+        actual = some_function(expected)
+
+        # assert
+        self.assertEqual(expected, actual)
