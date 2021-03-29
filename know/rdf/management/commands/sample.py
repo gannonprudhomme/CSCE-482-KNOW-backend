520faac494a18c042eff06be2bd91aa10bd3a7b1
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-03-29 15:06:23 -0500

    Added docstrings to function

--- know/rdf/management/commands/sample.py
+++ know/rdf/management/commands/sample.py
@@ -5,12 +5,12 @@ class Command(BaseCommand):
         Execute it like python manage.py sample -q query.sparql -u q:12345
     """
     def add_arguments(self, parser):
-        """ """
+        """ Adds arguments to the command """
         parser.add_argument('--query', '-q', type=str, help="describe query")
         parser.add_argument('--uri', '-u', type=str, help="describe uri")
 
     def handle(self, *_, **options):
-        """ """
+        """ Execute the query """
         query = options['query']
         if not query:
             raise Exception("please pass a query")
