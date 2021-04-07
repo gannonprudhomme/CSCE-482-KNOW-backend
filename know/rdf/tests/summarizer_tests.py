31326ec33fcc6e68f44a556466794f82faf42741
Nikolai Thompson <nickthompson234@gmail.com>
2021-04-07 01:28:57 -0500

    linter

--- know/rdf/tests/summarizer_tests.py
+++ know/rdf/tests/summarizer_tests.py
@@ -1,8 +1,9 @@
+# pylint: disable=wrong-import-position
 import sys
 sys.path.insert(1, "../parser")
 import unittest
-from summarizer import Summarizer
 from urllib.error import HTTPError
+from summarizer import Summarizer
 
 class SummarizerTester(unittest.TestCase):
     """summarizer test case """
@@ -20,21 +21,18 @@ class SummarizerTester(unittest.TestCase):
         # act
         summer = Summarizer('http://viaf.org/viaf/75121530/')
         actual = summer.get_wikidata_uri()
-        
+
 
         # assert
         self.assertEqual(expected, actual)
-    
+
     def test_summarizer_raises_exception_for_bad_uri(self):
         """ Tests if summarizer finds Albert Einstein's wikidata URI.
         """
 
-        # arrange
-        expected = 'http://www.wikidata.org/entity/Q937'
-
         # act
         summer = Summarizer('http://viaf.org/viaf/75121530098098/') #invalid URI
-        
+
 
         # assert
         self.assertRaises(HTTPError, summer.get_wikidata_uri)
