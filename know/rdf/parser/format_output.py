f6e4a989f0d70d37b48b3841144412377c86c79b
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-04-12 16:26:38 -0500

    Fixed linting

--- know/rdf/parser/format_output.py
+++ know/rdf/parser/format_output.py
@@ -1,6 +1,8 @@
 from typing import Union
 
-def format_output(title: str, subtitle: str, description: Union[str, None], entries: dict) -> dict:
+def format_output(
+    title: str, subtitle: str, description: Union[str, None], entries: dict
+) -> dict:
     """ Standardizes the response that is sent to the frontend. Should be called in the
         parse_book, parse_person, etc. functions
     """
