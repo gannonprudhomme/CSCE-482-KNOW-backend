a644c91ca4f03325b31a2690c8a53ebd0227615d
Gannon Prudhomme <gannonprudhomme@users.noreply.github.com>
2021-04-12 18:23:29 -0500

    Added format_output (#53)
    * Added format_output
    
    * Fixed linting

---
+++ know/rdf/parser/format_output.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+def format_output(
+    title: str, subtitle: str, description: Union[str, None], entries: dict
+) -> dict:
+    """ Standardizes the response that is sent to the frontend. Should be called in the
+        parse_book, parse_person, etc. functions
+    """
+    data = {
+        'title': title,
+        'subtitle': subtitle,
+        'entries': entries
+    }
+
+    if description: # Only add description if it's not None
+        data['description'] = description
+
+    return data
