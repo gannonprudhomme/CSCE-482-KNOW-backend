c1176bcfe6e61c723d0cd86a60be5b7ed57b1f81
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-04-14 14:29:25 -0500

    Fixed Nikolai's mistakes cause he's slow
    - Also added retrying
    - Updated test_wikidata command to call Summarizer

--- know/rdf/parser/sparql_reader.py
+++ know/rdf/parser/sparql_reader.py
@@ -1,9 +1,8 @@
 import sys
 import os
 
-
-def read_sparql(query_file: str, q_id: str) -> str:
-    """ Reads the .sparql file and replaces $0 with "Q" + entity id """
+def read_sparql(query_file: str, entity_id: str) -> str:
+    """ Reads the .sparql file and replaces $0 with the given entity ID"""
     relative_path = sys.path[0]
     path_name = os.path.join(relative_path, "rdf", "queries", "wikidata", query_file)
     query_string = ""
@@ -11,7 +10,7 @@ def read_sparql(query_file: str, q_id: str) -> str:
     for line in query_input:
         if line.find("#") == -1:
             if line.find("$0") != -1:
-                query_string += line.replace("$0", q_id)
+                query_string += line.replace("$0", entity_id)
             else:
                 query_string += line
     return query_string
--- know/rdf/tests/wikidata_tests.py
+++ know/rdf/tests/wikidata_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from rdf.parser.sparql_reader import read_sparql
-from rdf.parser.wikidata_parser import WikidataParser
+from rdf.parser.wikidata_parser import WikidataParser, UnsupportedEntityTypeException
 from rdf.parser.abstract_parser import EntityType
 
 
@@ -41,23 +41,9 @@ class ReadSparqlTests(unittest.TestCase):
         actual = parser.get_entity_type()
         self.assertEqual(expected, actual)
 
-    def test_fails_on_unsupported_entity_type(self):
+    def test_throws_UnsupportedEntityTypeException_on_unsupported_entity_type(self): # pylint: disable=invalid-name
         """ Tests if the wikidata parser raises an exception
             when the entity is not a type we can parse.
         """
         parser = WikidataParser('https://www.wikidata.org/wiki/Q6928344')
-        self.assertRaises(Exception, parser.get_entity_type())
-
-    def test_parse_book_returns_something(self):
-        """ Makes sure the parse_book function doesn't just throw an error
-        """
-        parser = WikidataParser('https://www.wikidata.org/wiki/Q43361')
-        actual = parser.parse_book()
-        self.assertTrue(actual is not None)
-
-    def test_parse_person_returns_something(self):
-        """ Makes sure the parse_person function doesn't just throw an error
-        """
-        parser = WikidataParser('https://www.wikidata.org/wiki/Q41513')
-        actual = parser.parse_person()
-        self.assertTrue(actual is not None)
+        self.assertRaises(UnsupportedEntityTypeException, parser.get_entity_type)
