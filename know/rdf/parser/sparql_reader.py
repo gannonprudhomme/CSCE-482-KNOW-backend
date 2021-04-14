227dd76f3b431840521e0366e3fd91c7686b4b99
Nikolai Thompson <nickthompson234@gmail.com>
2021-04-14 14:31:47 -0500

    wikidata parser works! (#46)
    * wikidata parser works!
    
    * tests
    
    * Fixed Nikolai's mistakes cause he's slow
    
    - Also added retrying
    - Updated test_wikidata command to call Summarizer
    
    Co-authored-by: Gannon Prudhomme <gannonprudhomme@gmail.com>

--- know/rdf/parser/sparql_reader.py
+++ know/rdf/parser/sparql_reader.py
@@ -1,9 +1,8 @@
 import sys
 import os
 
-
-def read_sparql(query_file: str, uri: str) -> str:
-    """ Reads the .sparql file and replaces $0 with the uri """
+def read_sparql(query_file: str, entity_id: str) -> str:
+    """ Reads the .sparql file and replaces $0 with the given entity ID"""
     relative_path = sys.path[0]
     path_name = os.path.join(relative_path, "rdf", "queries", "wikidata", query_file)
     query_string = ""
@@ -11,7 +10,7 @@ def read_sparql(query_file: str, uri: str) -> str:
     for line in query_input:
         if line.find("#") == -1:
             if line.find("$0") != -1:
-                query_string += line.replace("$0", uri)
+                query_string += line.replace("$0", entity_id)
             else:
                 query_string += line
     return query_string
--- know/rdf/tests/wikidata_tests.py
+++ know/rdf/tests/wikidata_tests.py
@@ -1,5 +1,7 @@
 import unittest
 from rdf.parser.sparql_reader import read_sparql
+from rdf.parser.wikidata_parser import WikidataParser, UnsupportedEntityTypeException
+from rdf.parser.abstract_parser import EntityType
 
 
 class ReadSparqlTests(unittest.TestCase):
@@ -21,3 +23,27 @@ class ReadSparqlTests(unittest.TestCase):
 
         # assert
         self.assertEqual(expected, actual)
+
+    def test_determines_book_entity_type(self):
+        """Tests if the wikidata parser correctly determines book entities are books
+        """
+        parser = WikidataParser('https://www.wikidata.org/wiki/Q43361')
+        expected = EntityType.BOOK
+        actual = parser.get_entity_type()
+        self.assertEqual(expected, actual)
+
+    def test_determines_person_entity_type(self):
+        """ Tests if the wikidata parser correctly determines
+            person entities are type person.
+        """
+        parser = WikidataParser('https://www.wikidata.org/wiki/Q41513')
+        expected = EntityType.PERSON
+        actual = parser.get_entity_type()
+        self.assertEqual(expected, actual)
+
+    def test_throws_UnsupportedEntityTypeException_on_unsupported_entity_type(self): # pylint: disable=invalid-name
+        """ Tests if the wikidata parser raises an exception
+            when the entity is not a type we can parse.
+        """
+        parser = WikidataParser('https://www.wikidata.org/wiki/Q6928344')
+        self.assertRaises(UnsupportedEntityTypeException, parser.get_entity_type)
