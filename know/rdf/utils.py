5b2c8f9c543d1f00c87ec511ca4cb3d1916a486c
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-04-14 15:38:45 -0500

    Linting

--- know/rdf/utils.py
+++ know/rdf/utils.py
@@ -1,5 +1,4 @@
 from dateutil.parser import parse
-from datetime import datetime
 
 def format_date_string(date_str: str) -> str:
     """ Converts a date string like 2019-09-01T07:58:30.996+0200 into
@@ -7,7 +6,7 @@ def format_date_string(date_str: str) -> str:
     """
     parsed_date = parse(date_str)
     # Windows and unix do non-leading zero dates differently, so handle both of them
-    win_date_format = "%B %#d, %Y" 
+    win_date_format = "%B %#d, %Y"
     unix_date_format = "%B %-d, %Y"
     try:
         return parsed_date.strftime(unix_date_format)
