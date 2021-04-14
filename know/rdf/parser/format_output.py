f8e02a788e0de35b6e7465fa9a308098df704b12
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-04-14 14:54:28 -0500

    Corrected format output

--- know/rdf/parser/format_output.py
+++ know/rdf/parser/format_output.py
@@ -1,10 +1,13 @@
-from typing import Union
+from typing import Union, Array
 
 def format_output(
-    title: str, subtitle: str, description: Union[str, None], entries: dict
+    title: str, subtitle: str, description: Union[str, None], entries: Array[dict]
 ) -> dict:
     """ Standardizes the response that is sent to the frontend. Should be called in the
         parse_book, parse_person, etc. functions
+
+        Note that each entry in the entries array should have a key-value pair with key 'key' and
+        key 'value', e.g. { 'key': 'Birth Date', 'value': 'September 8th, 1998' }
     """
     data = {
         'title': title,
